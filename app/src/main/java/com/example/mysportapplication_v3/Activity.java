1972f9f0781c68e66f23a2c7daff936e7759ab67
jpoireau <72388830+jpoireau@users.noreply.github.com>
2021-03-19 20:06:23 +0100

    update BDD

--- app/src/main/java/com/example/mysportapplication_v3/Activity.java
+++ app/src/main/java/com/example/mysportapplication_v3/Activity.java
@@ -2,13 +2,36 @@ package com.example.mysportapplication_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity extends AppCompatActivity {
 
+    Button desc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_);
+
+        desc = (Button) findViewById(R.id.button5);
+
+        desc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToConnexionBDD();
+            }
+        });
+    }
+
+    private void moveToConnexionBDD() {
+        Intent intent = new Intent(Activity.this, connexionBDD.class);
+        startActivity(intent);
     }
 }
+
+
+
+
+
